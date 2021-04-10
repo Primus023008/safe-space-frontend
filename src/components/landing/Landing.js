04637bf64e72d8df17ce2241a9305b7273e6fe38
Ishita Kabra <ishitakabra1805@gmail.com>
2021-04-10 12:17:17 +0530

    login comp

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -3,32 +3,25 @@ import "./landing.css"
 import vid1 from '../../assets/vid1.mp4'
 import img from '../../assets/Logo1.png'
 import Button from '../button/Button.js'
-
+import Login from '../login/Login';
 const Landing = () => {
     return (
         <div className='mainlanding'>
             <div className='leftlanding'>
-                {/* <div className='phone'> */}
-                    {/* <div className='overlay'></div>
-                    <video autoPlay muted>
-                        <source src={vid1} type="video/mp4" />
-                    </video>
-                    
-                </div> */}
-                <div className={`video-card`}>
+                <div className='video-card'>
                     <video loop autoPlay muted>
                         <source src={vid1} type="video/mp4"/>
                     </video>
                     <div className='vid-card-content'>
-                    Hello
+                        <Login/>
                     </div>
                 </div>
                 
             </div>
-            <div className='rightlanding'>
+            <div className='rightlanding hide'>
                 <img alt='logo' src={img}/>
                 <p id="h1">Your interactive <br/>self-transformation journey</p>
-                <p id="sub">An open source initiative by Ishita Kabra</p>
+                <p id="sub">An open source initiative by Ishita and Anushrav</p>
                 <Button label="Get Started"/>
             </div>
         </div>
